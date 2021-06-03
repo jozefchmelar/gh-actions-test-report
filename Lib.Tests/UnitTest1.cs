c7b028cdbded9231424b8203135336eab1dbb453
Jokinko <jozefchmelar@users.noreply.github.com>
2021-06-03 13:26:05 +0200

    init

---
+++ Lib.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+
+namespace Lib.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void First_test()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Second_test()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
---
+++ Lib/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Lib
+{
+    public class ToTests
+    {
+        public bool IsTrue => true;
+        public bool IsFalse => false;
+    }
+}
